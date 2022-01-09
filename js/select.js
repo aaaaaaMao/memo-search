1099a87d57561ccf04f6f1b24aa7c802c7d73aac
maojie <j.mao@maimemo.com>
2022-01-10 00:08:33 +0800

    feat: Add content block

--- js/select.js
+++ js/select.js
@@ -1,11 +1,28 @@
 const div = document.createElement('div')
-div.innerHTML = `
-<img id="icon" src="https://www.maimemo.com/res/images/site/logo4.png" style="width: 40px; height: 40px"/>
-`
 div.style.position = 'absolute'
 div.style.display = 'none'
+div.style.alignItems = 'flex-start'
 
+const icon = document.createElement('img')
+icon.id = 'icon'
+icon.src = 'https://www.maimemo.com/res/images/site/logo4.png'
+icon.style.width = '40px'
+icon.style.height = '40px'
+
+div.appendChild(icon)
 document.body.appendChild(div)
+
+const contentDiv = document.createElement('div')
+contentDiv.id = 'content'
+contentDiv.style.background = '#469F87'
+contentDiv.style.marginLeft = '10px'
+contentDiv.style.display = 'none'
+contentDiv.style.borderRadius = '5px'
+contentDiv.style.color = '#FFFFFF'
+contentDiv.style.fontSize = '16px'
+contentDiv.style.padding = '4px'
+
+div.appendChild(contentDiv)
 
 window.onmouseup = function(event) {
     const selection = window.getSelection();
@@ -13,11 +30,37 @@ window.onmouseup = function(event) {
     const srcElement = event.srcElement
     if (srcElement.id !== 'icon') {
         if (content) {
-            div.style.top = `${event.clientY + document.documentElement.scrollTop + document.body.scrollTop}px`
-            div.style.left = `${event.clientX + 10 + document.documentElement.scrollLeft + document.body.scrollLeft}px`
-            div.style.display = 'inherit'
+            const coord = getCoord({
+                x: event.clientX,
+                y: event.clientY
+            }, {
+                x: 10,
+                y: 0
+            })
+
+            div.style.top = `${coord.y}px`
+            div.style.left = `${coord.x}px`
+            div.style.display = 'flex'
+
+            contentDiv.innerHTML = content
         } else {
             div.style.display = 'none'
         }
     }
+    contentDiv.style.display = 'none'
+}
+
+function getCoord(base, offset) {
+    return {
+        x: base.x + offset.x + document.documentElement.scrollLeft + document.body.scrollLeft,
+        y: base.y + offset.y + document.documentElement.scrollTop + document.body.scrollTop
+    }
+}
+
+icon.onclick = function(event) {
+    const width = Math.floor(Math.random() * 100) + 200
+    const height = Math.floor(Math.random() * 100) + 220
+    contentDiv.style.width = `${width}px`
+    contentDiv.style.height = `${height}px`
+    contentDiv.style.display = 'inline-block'
 }
